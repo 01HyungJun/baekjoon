397d267dda06fa9cbdcf81fb6d533e5d387a05f9
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2024-01-11 21:20:33 +0900

    [Bronze I] Title: 유레카 이론, Time: 44 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <set>
+#include <math.h>
+#define fastio ios::sync_with_stdio(false), cin,tie(NULL), cout.tie(NULL)
+using namespace std;
+
+void calTriNum(set<int>& triNum) {
+	for (int i = 1; i <= (int)sqrt(2000); i++) {
+		triNum.insert((i * (i + 1)) / 2);
+	}
+}
+
+int main() {
+	int T, num = 0;
+	set<int> triNum;
+	calTriNum(triNum);
+	cin >> T;
+	
+	for (int i = 0; i < T; i++) {
+		bool flag = false;
+		cin >> num;
+		for (auto& it : triNum) {
+			for (auto& it2 : triNum) {
+				if (triNum.end() != triNum.find(num - it - it2)) {
+					cout << 1 << "\n";
+					flag = true;
+					break;
+				}
+			}
+			if (flag) break;
+		}
+		if (!flag) cout << 0 << "\n";
+	}
+}
