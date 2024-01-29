4b2a602f897a54c69243d136c27bb279a1fbbc23
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2024-01-29 21:25:22 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 328 ms, Memory: 6384 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Graph {
+public:
+	int N;
+
+	vector<vector<int>> adj;
+	vector<bool> visited;
+
+	Graph(int n, int m) : N(n) {
+		adj.resize(N + 1);
+		visited.resize(N + 1, false);
+		for (int i = 0; i < m; i++) {
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		sortList();
+	}
+
+	~Graph() {
+		vector<vector<int>>().swap(adj);
+		vector<bool>().swap(visited);
+	}
+
+	void sortList() {
+		for (int i = 0; i <= N; i++) sort(adj[i].begin(), adj[i].end());
+	}
+
+	int dfsAll() {
+		int component = 0;
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i]) {
+				dfs(i);
+				component++;
+			}
+		}
+		return component++;
+	}
+
+private:
+	void dfs(int curr) {
+		visited[curr] = true;
+		for (int& next : adj[curr]) {
+			if(!visited[next]) dfs(next);
+		}
+	}
+};
+
+int main() {
+	int N, M;
+
+	cin >> N >> M;
+
+	Graph* g = new Graph(N, M);
+	cout << g->dfsAll();
+	delete g;
+}
