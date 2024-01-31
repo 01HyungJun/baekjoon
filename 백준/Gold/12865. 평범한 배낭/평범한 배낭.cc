652bcbef592b3a3ffc1136856b6eaf6b04c2b0c6
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2024-01-31 19:59:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 52 ms, Memory: 42136 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N, K, maxV;
+	cin >> N >> K;
+
+	vector<vector<int>> package(N, vector<int>(2, -1));
+	vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
+
+	for (int i = 0; i < N; i++) {
+		cin >> package[i][0] >> package[i][1];
+	}
+
+	vector<int>(K + 1, 0).swap(dp[0]);
+	sort(package.begin(), package.end());
+
+	for (int i = 0; i < N; i++) {	// 물건 종류
+		for (int j = 0; j <= K; j++) {	// 물건 가치
+			dp[i + 1][j] = max(dp[i + 1][j], dp[i][j]);
+			int temp = j + package[i][0];
+			if (temp <= K) dp[i + 1][temp] = max(dp[i][temp], dp[i][j] + package[i][1]);
+		}
+	}
+
+	cout << dp[N][K];
+}
