8f101b98eedc34723c316a4de92e309bde3caaf1
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2024-02-03 02:06:20 +0900

    [Gold V] Title: 문자열 판별, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/16500. 문자열 판별/문자열 판별.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	string s;
+	int N;
+	cin >> s;
+	cin >> N;
+
+	vector<bool> dp(s.length() + 1, false);
+	vector<string> A;
+	string temp;
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+		A.push_back(temp);
+	}
+
+	int found = 0;
+	dp[0] = true;
+	for (int j = 0; j < s.length(); j++) {
+		for (int i = 0; i < N; i++) {
+			if (dp[j] == true) {
+				found = s.find(A[i], j);
+				if (found == j) dp[found + A[i].length()] = true;
+			}
+		}
+	}
+	
+	if (dp[s.length()]) cout << 1;
+	else cout << 0;
+}
