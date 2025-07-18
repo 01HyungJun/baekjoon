2c642c6da7c63b36a938208ef6b878688b0f44c4
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2025-07-18 18:01:22 +0900

    [level 0] Title: 배열 만들기 2, Time: 16.77 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,21 +1,31 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
+// import java.util.List;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int l, int r) {
-        List<Integer> list = new ArrayList<>();
+//         List<Integer> list = new ArrayList<>();
         
-        for (int i = l; i <= r; i++) {
-            boolean flag = true;
-            for (char ch : String.valueOf(i).toCharArray()) {
-                if (ch != '0' && ch != '5') {
-                    flag = false;
-                    break;
+//         for (int i = l; i <= r; i++) {
+//             boolean flag = true;
+//             for (char ch : String.valueOf(i).toCharArray()) {
+//                 if (ch != '0' && ch != '5') {
+//                     flag = false;
+//                     break;
+//                 }
+//             }
+//             if (flag) { list.add(i); }
+//         }
+        
+//         return list.isEmpty() ? new int[]{-1} : list.stream().mapToInt(Integer::intValue).toArray();
+        int[] array = IntStream.rangeClosed(l, r)
+            .filter(i -> {
+                while (i > 0) {
+                    if (i % 5 != 0) return false;
+                    i /= 10;
                 }
-            }
-            if (flag) { list.add(i); }
-        }
-        
-        return list.isEmpty() ? new int[]{-1} : list.stream().mapToInt(Integer::intValue).toArray();
+                return true;
+            }).toArray();
+        return array.length == 0 ? new int[]{-1} : array;
     }
 }
