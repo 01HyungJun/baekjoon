a2c395ec5f95422955b0ccf35478724c88edd451
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2025-07-18 15:16:29 +0900

    [level 0] Title: l로 만들기, Time: 5.48 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String myString) {
+        char[] ch = myString.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] < 'l') ch[i] = 'l';
+        }
+        return String.valueOf(ch);
+    }
+}
