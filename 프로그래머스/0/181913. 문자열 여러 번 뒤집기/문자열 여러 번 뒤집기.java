1b67d09763e9501a0b50320999a6b5a25a7a350e
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2025-07-18 16:12:57 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 6.22 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -3,10 +3,10 @@ class Solution {
         char[] ch = my_string.toCharArray();
         for(int[] t : queries) {
             boolean isEven = (t[1] - t[0] + 1) % 2 == 0;
-            for (int i = 0; i < (t[1] - t[0] + 1) / 2; i++) {
-                char temp = ch[t[0] + i];
-                ch[t[0] + i] = ch[t[1] - i];
-                ch[t[1] - i] = temp;
+            for (; t[1] > t[0]; t[0]++, t[1]--) {
+                char temp = ch[t[0]];
+                ch[t[0]] = ch[t[1]];
+                ch[t[1]] = temp;
             }
         }
         return String.valueOf(ch);
