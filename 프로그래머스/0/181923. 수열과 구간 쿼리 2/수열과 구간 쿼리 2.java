63c37e2e30de3ee267d87b098519deb9c1cb6227
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2025-07-18 14:45:48 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 8.47 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -5,13 +5,13 @@ class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         List<Integer> list = new ArrayList<>();
         for(int[] t : queries) {
-            int m = 1000001;
+            int m = Integer.MAX_VALUE;
             for (int i = t[0]; i <= t[1]; i++) {
                 if ((arr[i] > t[2]) && (m > arr[i])) {
                     m = arr[i];
                 }
             }
-            if (m != 1000001) list.add(m);
+            if (m != Integer.MAX_VALUE) list.add(m);
             else list.add(-1);
         }
         return list.stream().mapToInt(Integer::intValue).toArray();
