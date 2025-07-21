3e460f2e86359ff4b4d824b4524c155a9f858233
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2025-07-21 11:02:41 +0900

    [level 0] Title: 카운트 업, Time: 0.02 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,6 +1,9 @@
-import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        return IntStream.rangeClosed(start_num, end_num).toArray();
+        int[] result = new int[end_num - start_num + 1];
+        for (int i = start_num; i <= end_num; i++) {
+            result[i - start_num] = i;
+        }
+        return result;
     }
 }
