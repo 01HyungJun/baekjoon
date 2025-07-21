72284af8b9667e924a2d19c801b2d8cad217a834
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2025-07-21 15:03:38 +0900

    [level 0] Title: 세로 읽기, Time: 0.11 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -1,8 +1,8 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < my_string.length(); i += m) {
-            sb.append(my_string.substring(i, i + m).charAt(c - 1));
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            sb.append(my_string.charAt(i));
         }
         return sb.toString();
     }
