2ca8716bb36fe436e27f0422fa5d7e702d35fff4
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2025-07-22 11:07:20 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,14 +1,23 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.Collectors;
+// import java.util.List;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        List<Integer> list = new ArrayList<>();
-        list.addAll(Arrays.stream(num_list).boxed().collect(Collectors.toList()));
-        List<Integer> answer = list.subList(n, num_list.length);
-        answer.addAll(list.subList(0, n));
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        // List<Integer> list = new ArrayList<>();
+        // list.addAll(Arrays.stream(num_list).boxed().collect(Collectors.toList()));
+        // List<Integer> answer = list.subList(n, num_list.length);
+        // answer.addAll(list.subList(0, n));
+        // return answer.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = new int[num_list.length];
+        for (int i = 0; i < num_list.length; i++) {
+            if (n == num_list.length) {
+                n = 0;
+            }
+            answer[i] = num_list[n];
+            n++;
+        }
+        return answer;
     }
 }
