d7a8b403c1005d48f4dc4d888f73e75e904405ed
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2025-07-24 15:57:00 +0900

    [level 0] Title: 할 일 목록, Time: 2.97 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -3,13 +3,17 @@ import java.util.ArrayList;
 import java.util.List;
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        List<String> list = new ArrayList<>();
+//         List<String> list = new ArrayList<>();
         
+//         for (int i = 0; i < todo_list.length; i++) {
+//             if (!finished[i]) list.add(todo_list[i]);
+//         }
+        
+//         return list.toArray(new String[0]);
+        String str = "";
         for (int i = 0; i < todo_list.length; i++) {
-            if (!finished[i]) list.add(todo_list[i]);
+            str += !finished[i] ? todo_list[i] + " " : "";
         }
-        
-        
-        return list.toArray(new String[0]);
+        return str.split(" ");
     }
 }
