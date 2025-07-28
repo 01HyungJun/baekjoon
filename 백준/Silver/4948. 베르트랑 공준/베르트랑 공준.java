1a26dc65bbb72c951279ae1b016375c2ea036050
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2025-07-28 16:12:18 +0900

    [Silver II] Title: 베르트랑 공준, Time: 140 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,32 +1,37 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        boolean[] isNotPrime = new boolean[123456 * 2 + 1];
-        isNotPrime[0] = true; isNotPrime[1] = true;
+        boolean[] isPrime = new boolean[123456 * 2 + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false; isPrime[1] = false;
 
         for (int i = 2; i < Math.sqrt(123456 * 2); i++) {
-            for (int j = 2; j < isNotPrime.length; j++) {
-                if (j / i != 1 && j % i == 0) {
-                    isNotPrime[j] = true;
+            if (isPrime[i]) {
+                for (int j = i * 2; j < isPrime.length; j += i) {
+                    isPrime[j] = false;
                 }
             }
+            
         }
 
+        StringBuilder sb = new StringBuilder();
         while (true) {
             int n = Integer.parseInt(br.readLine());
             if (n == 0) break;
 
             int cnt = 0;
             for (int i = n + 1; i <= 2 * n; i++) {
-                if (!isNotPrime[i]) cnt++;
+                if (isPrime[i]) cnt++;
             }
-
-            System.out.println(cnt);
+            sb.append(cnt).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
