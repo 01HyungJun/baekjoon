556c2a856def41cee1ade1556dbbdd799113bdf3
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2025-07-28 12:02:44 +0900

    [Bronze I] Title: 최소공배수, Time: 124 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -17,23 +15,20 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            List<Integer> a_factor = new ArrayList<>();
-            int gcd = 1;
-            for (int j = 1; j <= a / 2; j++) {
-                if (a % j == 0) {
-                    a_factor.add(a / j);
-                }
-            }
-            for (int k = 1; k <= b / 2; k++) {
-                if (b % k == 0 && a_factor.contains(b / k)) {
-                    gcd = b / k;
-                    break;
-                }
-            }
-
-            sb.append(a * b / gcd).append("\n");
+            sb.append(LCM(a, b)).append("\n");
         }
 
         System.out.println(sb);
+    }
+
+    public static int GCD(int num1, int num2) {
+        if (num1 % num2 == 0) {
+            return num2;
+        }
+        return GCD(num2, num1 % num2);
+    }
+
+    public static int LCM(int num1, int num2) {
+        return num1 * num2 / GCD(num1, num2);
     }
 }
