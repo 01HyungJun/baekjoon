25b04400e24fe25882bc7e480636a413bb9092f2
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2025-08-05 11:58:57 +0900

    [level 0] Title: 세 개의 구분자, Time: 26.96 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,30 +1,43 @@
-import java.util.List;
-import java.util.ArrayList;
+// import java.util.List;
+// import java.util.ArrayList;
+import java.util.StringTokenizer;
 class Solution {
     public String[] solution(String myStr) {
-        List<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
+//         List<String> list = new ArrayList<>();
+//         StringBuilder sb = new StringBuilder();
         
-        for (char ch : myStr.toCharArray()) {
-            switch (ch) {
-                case 'a': case 'b': case 'c':
-                    if (sb.length() != 0) {
-                        list.add(sb.toString());
-                        sb.setLength(0);
-                    }
-                    break;
-                default:
-                    sb.append(ch);
-                    break;
+//         for (char ch : myStr.toCharArray()) {
+//             switch (ch) {
+//                 case 'a': case 'b': case 'c':
+//                     if (sb.length() != 0) {
+//                         list.add(sb.toString());
+//                         sb.setLength(0);
+//                     }
+//                     break;
+//                 default:
+//                     sb.append(ch);
+//                     break;
+//             }
+//         }
+//         if (sb.length() != 0) {
+//             list.add(sb.toString());
+//             sb.setLength(0);
+//         }
+//         if (list.size() == 0) {
+//             return new String[]{"EMPTY"};
+//         }
+//         return list.toArray(new String[0]);
+        String[] answer;
+        StringTokenizer st = new StringTokenizer(myStr, "abc");
+        if (st.hasMoreTokens()) {
+            answer = new String[st.countTokens()];
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = st.nextToken();
             }
         }
-        if (sb.length() != 0) {
-            list.add(sb.toString());
-            sb.setLength(0);
-        }
-        if (list.size() == 0) {
+        else {
             return new String[]{"EMPTY"};
         }
-        return list.toArray(new String[0]);
+        return answer;
     }
 }
