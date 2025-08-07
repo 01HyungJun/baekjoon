6aeff38a4745b283a16af6b3dcdac61ec5044fe3
01HyungJun <126967574+01HyungJun@users.noreply.github.com>
2025-08-07 17:51:28 +0900

    [Gold III] Title: 감시, Time: 168 ms, Memory: 19924 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,112 +1,52 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.List;
 import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class Main {
-    static int n, m;
-    static int min = Integer.MAX_VALUE;
-    static int cam_num = 0;
+    static int n, m, min = Integer.MAX_VALUE;
     static int[][] room;
+    static int[] dy = {-1, 0, 1, 0}, dx = {0, 1, 0, -1};
+    static List<int[]> cctv = new ArrayList<>();
+    static int[][][] camType = {
+        {},
+        { { 0 }, { 1 }, { 2 }, { 3 } },
+        { { 0, 2 }, { 1, 3 } },
+        { { 0, 1 }, { 1, 2 }, { 2, 3 }, { 3, 0 } },
+        { { 0, 1, 2 }, { 1, 2, 3 }, { 2, 3, 0 }, { 3, 0, 1 } }
+    };
 
-    public static void cam5(int y, int x) {
-        for (int i = y; i >= 0; i--) {
-            if (room[i][x] == 6) break;
-            if (room[i][x] == 0) {
-                room[i][x] = -1;
-            }
-        }
-        for (int i = y; i < n; i++) {
-            if (room[i][x] == 6) break;
-            if (room[i][x] == 0) {
-                room[i][x] = -1;
-            }
-        }
-        for (int i = x; i >= 0; i--) {
-            if (room[y][i] == 6) break;
-            if (room[y][i] == 0) {
-                room[y][i] = -1;
-            }
-        }
-        for (int i = x; i < m; i++) {
-            if (room[y][i] == 6) break;
-            if (room[y][i] == 0) {
-                room[y][i] = -1;
+    public static void camSite(int y, int x, int[] direct, int val) {
+        for (int d : direct) {
+            int i = y;
+            int j = x;
+            while (i >= 0 && i < n && j >= 0 && j < m) {
+                if (room[i][j] == 6) {
+                    break;
+                }
+                else if (room[i][j] <= 0) {
+                    room[i][j] += val;
+                }
+
+                i += dy[d];
+                j += dx[d];
             }
         }
     }
 
-    public static void cam(int y, int x, int[] arr) {
-        for (int num : arr) {
-            if (num == 0) {
-                for (int i = y; i >= 0; i--) {
-                    if (room[i][x] == 6) break;
-                    if (room[i][x] <= 0) {
-                        room[i][x]--;
-                    }
-                }
-            }
-            else if (num == 1) {
-                for (int i = x; i < m; i++) {
-                    if (room[y][i] == 6) break;
-                    if (room[y][i] <= 0) {
-                        room[y][i]--;
-                    }
-                }
-            }
-            else if (num == 2) {
-                for (int i = y; i < n; i++) {
-                    if (room[i][x] == 6) break;
-                    if (room[i][x] <= 0) {
-                        room[i][x]--;
-                    }
-                }
-            }
-            else if (num == 3) {
-                for (int i = x; i >= 0; i--) {
-                    if (room[y][i] == 6) break;
-                    if (room[y][i] <= 0) {
-                        room[y][i]--;
-                    }
-                }
-            }
+    public static void dfs(int cctvCnt) {
+        if (cctvCnt == cctv.size()) {
+            min = Math.min(min, count());
+            return;
         }
-    }
-
-    public static void cam_inc(int y, int x, int[] arr) {
-        for (int num : arr) {
-            if (num == 0) {
-                for (int i = y; i >= 0; i--) {
-                    if (room[i][x] == 6) break;
-                    if (room[i][x] < 0) {
-                        room[i][x]++;
-                    }
-                }
-            }
-            else if (num == 1) {
-                for (int i = x; i < m; i++) {
-                    if (room[y][i] == 6) break;
-                    if (room[y][i] < 0) {
-                        room[y][i]++;
-                    }
-                }
-            }
-            else if (num == 2) {
-                for (int i = y; i < n; i++) {
-                    if (room[i][x] == 6) break;
-                    if (room[i][x] < 0) {
-                        room[i][x]++;
-                    }
-                }
-            }
-            else if (num == 3) {
-                for (int i = x; i >= 0; i--) {
-                    if (room[y][i] == 6) break;
-                    if (room[y][i] < 0) {
-                        room[y][i]++;
-                    }
-                }
-            }
+        
+        int[] temp = cctv.get(cctvCnt);
+        for (int[] dir : camType[room[temp[0]][temp[1]]]) {
+            camSite(temp[0], temp[1], dir, -1);
+            dfs(cctvCnt + 1);
+            camSite(temp[0], temp[1], dir, 1);
         }
     }
 
@@ -114,77 +54,18 @@ public class Main {
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (room[i][j] == 0) cnt++;
+                if (room[i][j] == 0) {
+                    cnt++;
+                }
             }
         }
         return cnt;
     }
 
-    public static void dfs(int y, int x, int depth) {
-        if (depth == 0) {
-            min = Math.min(min, count());
-            return;
-        }
-
-        if (x == m) {
-            y++;
-            x = 0;
-        }
-
-        int i = y;
-        int j = x;
-        while (i < n) {
-            while (j < m) {
-                if (room[i][j] == 1 || room[i][j] == 2 || room[i][j] == 3 || room[i][j] == 4) {
-                    switch (room[i][j]) {
-                        case 1:
-                            for (int k = 0; k <= 3; k++) {
-                                cam(i, j, new int[]{k});
-                                dfs(i, j + 1, depth - 1);
-                                cam_inc(i, j, new int[]{k});
-                            }
-                            break;
-
-                        case 2:
-                            for (int k = 0; k <= 1; k++) {
-                                cam(i, j, new int[]{k, k + 2});
-                                dfs(i, j + 1, depth - 1);
-                                cam_inc(i, j, new int[]{k, k + 2});
-                            }
-                            break;
-
-                        case 3:
-                            for (int k = 0; k <= 3; k++) {
-                                cam(i, j, new int[]{k, (k + 1) % 4});
-                                dfs(i, j + 1, depth - 1);
-                                cam_inc(i, j, new int[]{k, (k + 1) % 4});
-                            }
-                            break;
-
-                        case 4:
-                            for (int k = 0; k <= 3; k++) {
-                                cam(i, j, new int[]{k, (k + 1) % 4, (k + 3) % 4});
-                                dfs(i, j + 1, depth - 1);
-                                cam_inc(i, j, new int[]{k, (k + 1) % 4, (k + 3) % 4});
-                            }
-                            break;
-
-                        default:
-                            break;
-                    }
-                    return;
-                }
-                j++;
-            }
-            i++;
-            j = 0;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
@@ -193,24 +74,25 @@ public class Main {
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int j = 0;
+
             while (st.hasMoreTokens()) {
-                room[i][j++] = Integer.parseInt(st.nextToken());
+                room[i][j] = Integer.parseInt(st.nextToken());
+                if (room[i][j] == 1 || room[i][j] == 2 || room[i][j] == 3 || room[i][j] == 4) {
+                    cctv.add(new int[]{i, j});
+                }
+                j++;
             }
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (room[i][j] == 5) {
-                    cam5(i, j);
-                }
-                else if (room[i][j] == 1 || room[i][j] == 2 || room[i][j] == 3 || room[i][j] == 4) {
-                    cam_num++;
+                    camSite(i, j, new int[]{0, 1, 2, 3}, -1);
                 }
             }
         }
 
-        dfs(0, 0, cam_num);
-
+        dfs(0);
         System.out.println(min);
     }
 }
